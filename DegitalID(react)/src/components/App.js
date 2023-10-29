36c3e18892d8c8442cc895596f777280a97d5cc3
ferdous_7272 <ferdous.frd81@gmail.com>
2023-10-29 23:52:13 +0200

    Added Emoji fighter and deleted advance-filtering

---
+++ DegitalID(react)/src/components/App.js
@@ -0,0 +1,14 @@
+import Header from "./Header"
+import MainContent from "./MainComponent"
+import Footer from "./Footer"
+
+function App(){
+    return(
+        <>
+            <Header />
+            <MainContent />
+            <Footer />
+        </>
+    )
+}
+export default App
--- project-1/src/components/Footer.js
+++
@@ -1,5 +0,0 @@
-function Footer(){
-    return(
-    <p>Thank You for visiting. Have a nice day!</p>)
-}
-export default Footer
