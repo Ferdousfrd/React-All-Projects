c87006980500b12affced736e5721aa6a3eb4c49
ferdous_7272 <ferdous.frd81@gmail.com>
2023-11-14 20:51:08 +0200

    added delete button and dblClick listener

--- ChromeExtension(vanilaJs)/index.js
+++ ChromeExtension(vanilaJs)/index.js
@@ -1,22 +1,32 @@
 const btnEl = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
 
 let myLeads = []
 
+//bringing the values from localstorage, storing them into a local variable and since they are strings, we parse them to make em array
+const leadsLocalStorage = JSON.parse( localStorage.getItem("myLeads") )   
+if(leadsLocalStorage){                  //checking localstoreage truthy or falsy(null if its empty)
+    myLeads = leadsLocalStorage         //assigning localstorage value to myLeads array
+    renderList()
+}
 
-let leadsLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-console.log(leadsLocalStorage)
 
-
-
-btnEl.addEventListener("click", function(){
+btnEl.addEventListener( "click", function(){
     myLeads.push(inputEl.value)
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
     inputEl.value = ""
     renderList()
 
 })
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    renderList()
+})
+
 
 function renderList(){
     let listItems = ""
